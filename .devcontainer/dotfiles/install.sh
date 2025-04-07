d38eab232400484b22a04062b2cc9fc50bbc290f
Gerard Braad <me@gbraad.nl>
2025-04-07 19:32:46 +0800

    Follow the container build strategy for dotfiles

--- .devcontainer/dotfiles/install.sh
+++ .devcontainer/dotfiles/install.sh
@@ -7,10 +7,39 @@ set -o allexport
 
 readonly NAME="dotfiles"
 
-install() {
-    curl -fsSL https://dotfiles.gbraad.nl/install.sh -o /usr/bin/install-dotfiles
-    chmod +x /usr/bin/install-dotfiles
+installuser() {
+    if [ "$_REMOTE_USER" = "root" ]; then
+        echo "Detected root to be the remote user - exiting ${NAME} installation."
+        return
+    fi
+    if ! id "$_REMOTE_USER"; then
+        echo "User does not exist - exiting ${NAME} installation."
+        return
+    fi
+
+    commands="rm -rf ~/.oh-my-zsh ~/.zshrc ; \
+        git clone https://github.com/gbraad-dotfiles/upstream.git ~/.dotfiles --depth 2 \
+            && cd ~/.dotfiles \
+            && bash ./install.sh"
+
+    su - $_REMOTE_USER -c "${commands}" \
+        && chsh -s /usr/bin/zsh $_REMOTE_USER
 }
 
-echo "Installing $NAME..."
+installroot() {
+    rm -rf ~/.oh-my-zsh ~/.zshrc
+    git clone https://github.com/gbraad-dotfiles/upstream.git ~/.dotfiles --depth 2 \
+        && cd ~/.dotfiles \
+        && . ./zsh/.zshrc.d/dotfiles.zsh \
+        && stow config \
+        && dotfiles restow \
+        && chsh -s /usr/bin/zsh
+}
+
+install() {
+    installuser
+    installroot
+}
+
+echo "Installing ${NAME}..."
 install "$@"
